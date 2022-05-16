161061d749289cb38ba98b1036c2fead751e0601
TaneriX07 <tanerivince@gmail.com>
2022-05-16 09:08:04 +0800

    Add styling

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div>
-      <h1>Rick and Morty</h1>
+    <div className="container">
+      <nav className="navbar sticky-top navbar-light bg-dark">
+        <h1 className="navbar-brand text-light">Rick and Morty</h1>
+      </nav>
       <List />
     </div>
   );
