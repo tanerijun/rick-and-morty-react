24aab14e903bbb6099254dfe809b2115d06621da
TaneriX07 <tanerivince@gmail.com>
2022-05-16 08:27:41 +0800

    Finish basic functionality

--- src/components/List.js
+++ src/components/List.js
@@ -1,5 +1,45 @@
+import { useState, useEffect } from 'react';
+import Character from './Character';
+
 function List() {
-  return <h2>Characters</h2>;
+  const [characters, setCharacters] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  // fetch data
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const data = await fetch('https://rickandmortyapi.com/api/character');
+
+        const { results } = await data.json();
+
+        setCharacters(results);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error from useEffect in List.js');
+      }
+    }
+
+    fetchData();
+  }, [characters.length]);
+
+  return (
+    <div>
+      <h2>Characters</h2>
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        characters.map((character) => (
+          <Character
+            key={character.id}
+            name={character.name}
+            origin={character.origin}
+            image={character.image}
+          />
+        ))
+      )}
+    </div>
+  );
 }
 
 export default List;
